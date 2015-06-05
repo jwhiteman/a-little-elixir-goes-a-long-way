0a3f3ac137e35d6c2ecebf2f0d87395a51e9433a
Jim Whiteman <jimtron9000@gmail.com>
2015-06-05 15:12:28 -0700

    Chapter 6.

--- lib/schemer/shadows.ex
+++ lib/schemer/shadows.ex
@@ -1,14 +1,155 @@
 defmodule Schemer.Shadows do
-  """
-  numbered?
-  value
-  1st-sub-expr
-  2nd-sub-expr
-  operator
+  @moduledoc """
+  The Seventh Commandment: Recur on the _subparts_ that are the same in nature:
+  - On the sublists of a list. 
+  - On the subexpressions of an arithmetic expression.
 
-  sero?
-  add1
-  +
-  lat?
+  The Eighth Commandment: Use help functions to abstract from representations.
   """
+
+  @doc """
+  (define numbered?
+    (lambda (e)
+      (cond
+        ((atom? e)(number? e))
+         (else
+           (and (or (number? (car e))
+                    (numbered? (car e)))
+                (or (eq? (car (cdr e)) '+)
+                    (eq? (car (cdr e)) '*)
+                    (eq? (car (cdr e)) '^))
+                (or (number? (car (cdr (cdr e))))
+                    (numbered? (car (cdr (cdr e))))))))))
+
+  (numbered? '(1 + 1))
+  => #t
+
+  (numbered? 1)
+  => #t
+
+  (numbered? '(3 + (4 * 5)))
+  => #t
+
+  (numbered? '(3 * sausage))
+  => #f
+  """
+  def numbered([left, operator, right]) do
+    numbered(left) && valid_operator(operator) && numbered(right)
+  end
+
+  def numbered(n), do: is_number(n)
+
+  defp valid_operator(:+), do: true
+  defp valid_operator(:*), do: true
+  defp valid_operator(:^), do: true
+  defp valid_operator(_), do: false
+
+  @doc """
+  (define value
+  (lambda (nexp)
+    (cond
+      ((atom? nexp) nexp)
+      ((eq? (car (cdr nexp)) '+)
+       (+ (value (car nexp))
+          (value (car (cdr (cdr nexp))))))
+      ((eq? (car (cdr nexp)) '*)
+       (* (value (car nexp))
+          (value (car (cdr (cdr nexp))))))
+      (else
+         (pow (value (car nexp))
+              (value (car (cdr (cdr nexp)))))))))
+
+  (value 13)
+  => 13
+
+  (value '(1 + 3))
+  => 4
+
+  (value '(1 + (3 ^ 4)))
+  => 82
+  """
+  def value([l, :+, r]), do: value(l) + value(r)
+  def value([l, :*, r]), do: value(l) * value(r)
+  def value([l, :^, r]), do: pow(value(l), value(r))
+  def value(n) when is_number(n), do: n
+
+  @doc """
+  (define 1st-sub-expression
+    (lambda (exp)
+      (car (cdr exp))))
+
+   (define 2nd-sub-expression
+     (lambda (exp)
+       (car (cdr (cdr exp)))))
+   
+   (define operator
+     (lambda (exp)
+       (car exp)))
+   
+   (define generic-value
+     (lambda (nexp)
+       (cond
+         ((atom? nexp) nexp)
+         ((eq? (operator nexp) '+)
+          (+ (generic-value (1st-sub-expression nexp))
+             (generic-value (2nd-sub-expression nexp))))
+         ((eq? (operator nexp) '*)
+          (* (generic-value (1st-sub-expression nexp))
+             (generic-value (2nd-sub-expression nexp))))
+         (else
+          (pow (generic-value (1st-sub-expression nexp))
+             (generic-value (2nd-sub-expression nexp)))))))
+  """
+  def generic_value(n) when is_number(n), do: n
+  def generic_value([_,_,_]=aexp) do
+    case operator_for(aexp) do
+      :+ -> first_sub_expression(aexp) + second_sub_expression(aexp)
+      :* -> first_sub_expression(aexp) * second_sub_expression(aexp)
+      :^ -> pow(first_sub_expression(aexp), second_sub_expression(aexp))
+    end
+  end
+
+  @doc """
+  (define sero?
+  (lambda (n)
+    (null? n)))
+
+  (define edd1
+    (lambda (n)
+      (cons '() n)))
+  
+  (define zub1
+    (lambda (n)
+      (cdr n)))
+  
+  (define sadd
+    (lambda (n m)
+      (cond
+        ((sero? m) n)
+        (else
+         (edd1 (sadd n (zub1 m)))))))
+  """
+  def sero([]), do: true
+  def sero(_), do: false
+
+  def edd1(l), do: [[]|l]
+
+  def zub1([_|t]), do: t
+
+  def sadd(n, m) do
+    cond do
+      sero(m) -> n
+      true    -> edd1(sadd(n, zub1(m)))
+    end
+  end
+
+  defp operator_for([_, :+, _]), do: :+
+  defp operator_for([_, :*, _]), do: :*
+  defp operator_for([_, :^, _]), do: :^
+
+  defp first_sub_expression([sub, _, _]), do: generic_value(sub)
+  defp second_sub_expression([_, _, sub]), do: generic_value(sub)
+
+  defp pow(_, 0), do: 1
+  defp pow(n, m), do: n * pow(n, m-1)
 end
--- test/shadows_test.exs
+++ test/shadows_test.exs
@@ -2,4 +2,28 @@ defmodule ShadowsTest do
   use ExUnit.Case
 
   import Schemer.Shadows
+
+  test "numbered" do
+    assert numbered([1, :+, 1]) == true
+    assert numbered(1) == true
+    assert numbered([3, :+, [4, :*, 5]]) == true
+    assert numbered([3, :*, :sausage]) == false
+  end
+
+  test "value" do
+    assert value(13) == 13
+    assert value([1, :+, 3]) == 4
+    assert value([1, :+, [3, :^, 4]]) == 82
+  end
+
+  test "generic_value" do
+    assert generic_value(13) == 13
+    assert generic_value([1, :+, 3]) == 4
+    assert generic_value([1, :+, [3, :^, 4]]) == 82
+  end
+
+  test "sadd" do
+    assert sero([]) == true
+    assert sadd([[], []], [[]]) == [[], [], []]
+  end
 end
