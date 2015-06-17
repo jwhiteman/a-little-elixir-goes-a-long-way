7261a289baa62d9ea79eb2f755f9b6607a640f1a
Jim Whiteman <jimtron9000@gmail.com>
2015-06-17 08:36:08 -0700

    Remove Parser.

--- lib/schemer/support/parser.ex
+++
@@ -1,7 +0,0 @@
-defmodule Schemer.Support.Parser do
-  def parse(str) do
-    {:ok, tokens, _} = str |> to_char_list |> :slexer.string
-    {:ok, list}      = :sparser.parse(tokens)
-    list
-  end
-end
