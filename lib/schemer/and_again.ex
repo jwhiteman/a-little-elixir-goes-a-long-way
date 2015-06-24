335a66d09e98a9601a8159773d31c70997c369fa
Jim Whiteman <jimtron9000@gmail.com>
2015-06-24 10:06:43 -0700

    Chapter 9 WIP.

--- lib/schemer/and_again.ex
+++ lib/schemer/and_again.ex
@@ -1,9 +1,9 @@
 # Chapter 9. WIP
 defmodule Schemer.AndAgain do
+
+  import Schemer.NumbersGames, only: [pick: 2]
+
   @moduledoc """
-  looking
-  keep-looking
-  eternity
   shift
   align
   length*
@@ -14,4 +14,47 @@ defmodule Schemer.AndAgain do
   last-try
   Y
   """
+
+  @doc """
+  (define looking
+    (lambda (a lat)
+      (keep-looking a (pick 1 lat) lat)))
+
+  (define keep-looking
+    (lambda (a sorn lat)
+      (cond
+        ((number? sorn)
+         (keep-looking a (pick sorn lat) lat))
+        (else
+          (eq? sorn a)))))
+
+  (looking 'caviar '(6 2 4 caviar 5 7 3))
+  => #t
+
+  (looking 'caviar '(6 2 grits caviar 5 7 3))
+  => #f
+  """
+  def looking(a, lat), do: keep_looking(a, pick(1, lat), lat)
+
+  defp keep_looking(a, sorn, lat) when is_number(sorn) do
+    keep_looking(a, pick(sorn, lat), lat)
+  end
+  defp keep_looking(a, a, _), do: true
+  defp keep_looking(_, _, _), do: false
+
+  @doc """
+  (define eternity
+    (lambda (x)
+      (eternity x)))
+  """
+  def eternity(x), do: eternity(x)
+
+  @doc """
+  (shift '((a b) c))
+  => (a (b c))
+
+  (shift '((a b) (c d)))
+  => (a (b (c d)))
+  """
+
 end
--- test/and_again_test.exs
+++ test/and_again_test.exs
@@ -2,4 +2,9 @@ defmodule AndAgainTest do
   use ExUnit.Case
 
   import Schemer.AndAgain
+
+  test "looking" do
+    assert looking(:caviar, [6, 2, 4, :caviar, 5, 7, 3])
+    refute looking(:caviar, [6, 2, :grits, :caviar, 5, 7, 3])
+  end
 end
